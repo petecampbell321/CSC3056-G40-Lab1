bcb0a9c9d78bc6bb51135dfa6750942e131f6c7a
Peter Campbell <pcampbell52@qub.ac.uk>
2022-02-16 13:25:32 +0000

    Added automated testing for UserTests

--- src/tests/UserTests.java
+++ src/tests/UserTests.java
@@ -3,11 +3,50 @@ import model.User;
 public class UserTests {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		User testUser = new User("mike","my_passwd","Mike","Smith","07771234567");
-		System.out.println(testUser);
+		/* OLD: manual testing
+		 * User testUser = new User("mike","my_passwd","Mike","Smith","07771234567");
+		 * System.out.println(testUser);
+		 */
 		
-
+		// Automated Testing
+		
+		// 1-Setup the object and test data
+		String testUsername = "mike";
+		String testPassword = "my_passwd";
+		String testFirstName = "Mike";
+		String testLastName = "Smith";
+		String testMobileNumber = "07771234567";
+		
+		// 2-Exercise, run the object under test
+		User testUser = new User(testUsername, testPassword, testFirstName, testLastName, testMobileNumber);
+		
+		// 3-Verify (Assert)
+		Boolean passed = true;
+		
+		if (testUser.getUsername() != testUsername) {
+			System.out.println("TC1 failed: username did not match");
+			passed = false;
+		}
+		if (testUser.getPassword() != testPassword) {
+			System.out.println("TC2 failed: password did not match");
+			passed = false;
+		}
+		if (testUser.getFirstName() != testFirstName) {
+			System.out.println("TC3 failed: first name did not match");
+			passed = false;
+		}
+		if (testUser.getLastName() != testLastName) {
+			System.out.println("TC4 failed: last name did not match");
+			passed = false;
+		}
+		if (testUser.getMobileNumber() != testMobileNumber) {
+			System.out.println("TC5 failed: mobile number did not match");
+			passed = false;
+		}
+		
+		if (passed) {
+			System.out.println("All TCs passed.");
+		}
 	}
 
 }
